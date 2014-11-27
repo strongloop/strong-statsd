2886dae13488d6314252851a7b974a40587b9d56
Sam Roberts <sam@strongloop.com>
2014-11-27 11:35:57 -0800

    Error if no backends, or multiple backends.

--- test/test-none.js
+++ test/test-none.js
@@ -1,27 +1,17 @@
 // Copyright (C) 2014 Strongloop, see LICENSE.md
 var assert = require('assert');
 var Statsd = require('../');
-var statsd = new Statsd({debug: true, silent: true, flushInterval: 2});
+var statsd = new Statsd();
+var tap = require('tap');
 
-assert.equal(statsd.port, 0);
-
-var metric = 'foo.count';
-
-// FIXME should refuse to start with no backends!
-statsd.start(function(er) {
-  console.log('started on', statsd.port);
-  if (er) throw er;
-  assert(statsd.port > 0);
-  console.log('send `%s` to %d', metric, statsd.port);
-  statsd.send(metric, 12);
-});
-
-statsd.child.stdout.on('data', function(data) {
-  if (data.indexOf(metric) >= 0) {
-    statsd.stop();
-  }
+tap.test('fail with no backends', function(t) {
+    t.plan(2);
+    statsd.start(function(er) {
+      t.assert(er);
+      t.equal(er.message, 'start failed: no backends configured');
+    });
 });
 
 process.on('exit', function(code) {
-  if (code == 0) console.log('PASS');
+  console.log('EXIT:', code);
 });
